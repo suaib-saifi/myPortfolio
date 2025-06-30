7467f1552dae62aa93bfd0b16cf9fbd326b92262
Suaib Saifi <suaeb2001@gmail.com>
2025-07-01 01:41:13 +0530

    second commit

--- src/Components/MyWork/MyWork.jsx
+++ src/Components/MyWork/MyWork.jsx
@@ -4,6 +4,9 @@ import theme_pattern from "../../assets/theme_pattern.svg";
 import project1_img from "../../assets/project1.png";
 import project2_img from "../../assets/project2.png";
 import project3_img from "../../assets/project3.png";
+import project4_img from "../../assets/project4.png";
+import project5_img from "../../assets/project5.png";
+import project6_img from "../../assets/project6.png";
 import arrow_icon from "../../assets/arrow_icon.svg";
 
 const MyWork = () => {
@@ -19,11 +22,30 @@ const MyWork = () => {
             <img src={project1_img} alt="" />
           </div>
         </a>
+        <a href="https://spotify-ui-clone-theta.vercel.app/">
+          <div className="box">
+            <img src={project4_img} alt="" />
+          </div>
+        </a>
+
+        <a href="https://restaura-wheat.vercel.app/">
+          <div className="box">
+            <img src={project5_img} alt="" />
+          </div>
+        </a>
+
+        <a href="https://vastu-spaze-one.vercel.app/">
+          <div className="box">
+            <img src={project6_img} alt="" />
+          </div>
+        </a>
+
         <a href="https://zayka-zone-ur86.vercel.app/">
           <div className="box">
             <img src={project2_img} alt="" />
           </div>
         </a>
+
         <a href="https://virtualr-sage.vercel.app/">
           <div className="box">
             <img src={project3_img} alt="" />
